d020496142af7894055d069fa9ac394e33b9c595
ArnoldC <arrnoldc@gmail.com>
2026-02-18 20:24:52 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/my-company-site/', // 修改這裡：換成您的 GitHub Repository 名稱，前後要有斜線
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
