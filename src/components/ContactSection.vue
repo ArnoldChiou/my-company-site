702acc7bb927834fb8aff68d23b7050213ea5a85
ArnoldC <arrnoldc@gmail.com>
2026-02-20 13:40:43 +0800

    新增統一編號資訊

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -80,6 +80,15 @@ const submitForm = async () => {
               </dt>
               <dd><a class="info-link" href="tel:0926192178">0926-192-178</a></dd>
             </div>
+            <div class="info-item">
+              <dt class="flex-none">
+                <span class="sr-only">統一編號</span>
+                <svg class="info-icon" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                </svg>
+              </dt>
+              <dd>統一編號：00884771</dd>
+            </div>
           </dl>
         </div>
 
